7f208ef8c8e6dea3da5e2432d5a6e2a81d161879
Maqhawe Mike Malindzisa <machaweml@gmail.com>
2023-07-27 02:21:51 +0200

    Update ncg_compose.php

--- main/core_functions/ncg_compose.php
+++ main/core_functions/ncg_compose.php
@@ -6,17 +6,16 @@ Here is a working version. I've also added two commented out lines, which you ca
 require 'vendor/autoload.php';
 $mail = new PHPMailer(true);
 $mail->isSMTP();
-//$mail->Host = 'smtp.office365.com';
 $mail->Port       = 587;
 $mail->SMTPSecure = 'tls';
 $mail->SMTPAuth   = true;
-//$mail->Username = 'no-reply@inyatsi.co.sz';
-$mail->Host       = 'smtp.outsourceszl.com';
-//$mail->Password = 'AllowMe2021!';                              //Enable SMTP authentication
-$mail->Username   = 'mike@outsourceszl.com';                     //SMTP username
-$mail->Password   = 'whocares27'; 
-$mail->SetFrom('no-reply@inyatsi.co.sz', 'Inyats Construction');
-$mail->addAddress('mike@outsourceszl.com', 'Mike Malindzisa');
+//$mail->Username = '';
+$mail->Host       = '';
+//$mail->Password = '';                              //Enable SMTP authentication
+$mail->Username   = '';                     //SMTP username
+$mail->Password   = ''; 
+$mail->SetFrom('', 'Inyats Construction');
+$mail->addAddress('', '');
 //$mail->SMTPDebug  = 3;
 //$mail->Debugoutput = function($str, $level) {echo "debug level $level; message: $str";}; //$mail->Debugoutput = 'echo';
 $mail->IsHTML(true);
